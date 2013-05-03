4e5aeff548da66bb0a20709c05fcf79da88229fe
Marius-Cristian Manole <manole.cristi@gmail.com>
2013-05-03 12:10:20 +0100

    Installed Bootstrap, Made a Register Form

---
+++ mockup/register.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Registration form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="../assets/js/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+        
+
+
+    <body>
+
+        <form action="" method="">
+            <fieldset>
+                <legend> Register now! </legend>
+                
+                <label> Your username </label>
+                <input type="text" placeholder="> " name="username">
+                <span class="help-block">Should be between 4-12 characters</span>
+
+                <label> Your e-mail </label>
+                <input type="text" placeholder="> " name="email">
+                <span class="help-block">Your primary e-mail address</span>
+
+                <label> Your password </label>
+                <input type="password" placeholder="> " name="email">
+                <span class="help-block">Choose a complex password</span>
+
+                <label> Retype password </label>
+                <input type="password" placeholder="> " name="email">
+                <span class="help-block">Retype your password</span>
+                
+                <button type="submit" class="btn">Register me!</button>
+
+            </fieldset>
+        </form>
+
+
+        <!-- jQuery -->
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+</html>
