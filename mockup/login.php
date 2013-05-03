ce30d72591db4f13b6f3f432078bea38c167a5e8
Marius-Cristian Manole <manole.cristi@gmail.com>
2013-05-03 12:39:59 +0100

    Login form created

---
+++ mockup/login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Registration form</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="../assets/js/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+        
+
+
+    <body>
+
+        <form class="form-horizontal" action="" method="">
+            <fieldset>
+                
+                <legend>Log in</legend>
+                
+                <div class="control-group">
+                    <label class="control-label" for="inputUser"> Username </label>
+                    <div class="controls">
+                        <input type="text" id="inputUser" placeholder="> " name="username">
+                    </div>
+                </div>
+
+                <div class="control-group">
+                    <label class="control-label" for="inputPass"> Your password </label>
+                    <div class="controls">
+                        <input type="password" id="inputPass" placeholder="> " name="password">
+                        <button type="submit" class="btn" style="display:block;margin-top:10px;">Log in</button>
+                    </div>
+                </div>
+                
+                
+
+            </fieldset>
+        </form>
+
+
+        <!-- jQuery -->
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+
+</html>
