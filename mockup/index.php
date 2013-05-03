9cc051ff5c45934c79497ee4d72a8fcd975e5e5c
Marius-Cristian Manole <manole.cristi@gmail.com>
2013-05-03 16:16:38 +0100

    Homepage alpha version released

--- mockup/index.php
+++ mockup/index.php
@@ -18,11 +18,99 @@
         
 
 
-    <body>
-        <h1>Hello, world!</h1>
+    <body style="background:url('img/pattern.png');border-radius:10px;">
+
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="container">
+                    <div class="nav">
+                        <a class="brand" href="index.php"> Our first blog </a>
+                        <ul class="nav">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="index.php">Link to ?</a></li>
+                            <li><a href="index.php">Link to ?</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+
+            <div class="row">
+                <div class="span12" style="margin-left:0;">
+                    <h2 style="color:orange;" align="center">Have no fear of perfection, as you will never reach it!</h2>
+                    <img src="img/header.jpg" class="img-polaroid" width="940" height="300" style="padding:10px;">
+                </div>
+            </div>
+
+            <div class="row" style="margin-top:20px;">
+                <div class="span8">
+
+                    <div style="background:#FFF;border-radius:5px;">
+                        <h2 align="center">Post title goes here</h2>
+                        
+                        <p class='lead' style="float:left;margin-left:4px;">03 May 2013</p>
+                        <p class='lead' style="float:right;margin-right:4px;">by&nbsp;<a href="#">Nelly Furtado</a></p>
+                        
+                        <p style="clear:both;padding:5px;" align="justify">
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor suscipit est, eu condimentum nisi commodo et. Vivamus quis purus erat. Donec consectetur tortor vitae neque consequat ac tempor ipsum tincidunt. Nullam pellentesque nulla sed felis elementum pellentesque. Quisque fringilla erat quis purus sagittis eu vulputate neque faucibus. Mauris at venenatis urna. Nulla in libero ipsum. Vivamus purus velit, convallis sed facilisis et, rutrum vitae nulla. Integer consequat elit at justo rhoncus accumsan.
+                            Phasellus sagittis hendrerit placerat. In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis porta blandit, diam nibh porta ante, et commodo erat eros vitae nibh. Nulla facilisi. Aenean eget sem a augue ultrices interdum. Nunc id mattis purus. Ut vulputate mollis augue a laoreet. Fusce mollis molestie erat, non vulputate sapien tincidunt sit amet. Praesent lobortis purus at tortor hendrerit sed aliquam purus adipiscing. Aliquam ante lacus, molestie id mattis quis, faucibus quis ante. Pellentesque odio elit, tincidunt sed luctus ac, blandit sit amet metus. Maecenas quis massa odio, quis tempus nulla. Aenean eros dolor, commodo id pretium nec, faucibus at velit. Aenean egestas odio ac massa sodales hendrerit. 
+                        </p>
+
+                        <p class="lead" style="margin-left:4px;">Tags: 
+                            <a href="#">tag 1</a>
+                            <a href="#">tag 2</a>
+                            <a href="#">tag 3</a>
+                        </p>
+                    </div>
+                    <!-- Last post div repeated, delete this one -->
+                    <div style="background:#FFF;border-radius:5px;">
+                        <h2 align="center">Post title goes here</h2>
+                        
+                        <p class='lead' style="float:left;margin-left:4px;">03 May 2013</p>
+                        <p class='lead' style="float:right;margin-right:4px;">by&nbsp;<a href="#">Nelly Furtado</a></p>
+                        
+                        <p style="clear:both;padding:5px;" align="justify">
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor suscipit est, eu condimentum nisi commodo et. Vivamus quis purus erat. Donec consectetur tortor vitae neque consequat ac tempor ipsum tincidunt. Nullam pellentesque nulla sed felis elementum pellentesque. Quisque fringilla erat quis purus sagittis eu vulputate neque faucibus. Mauris at venenatis urna. Nulla in libero ipsum. Vivamus purus velit, convallis sed facilisis et, rutrum vitae nulla. Integer consequat elit at justo rhoncus accumsan.
+                            Phasellus sagittis hendrerit placerat. In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis porta blandit, diam nibh porta ante, et commodo erat eros vitae nibh. Nulla facilisi. Aenean eget sem a augue ultrices interdum. Nunc id mattis purus. Ut vulputate mollis augue a laoreet. Fusce mollis molestie erat, non vulputate sapien tincidunt sit amet. Praesent lobortis purus at tortor hendrerit sed aliquam purus adipiscing. Aliquam ante lacus, molestie id mattis quis, faucibus quis ante. Pellentesque odio elit, tincidunt sed luctus ac, blandit sit amet metus. Maecenas quis massa odio, quis tempus nulla. Aenean eros dolor, commodo id pretium nec, faucibus at velit. Aenean egestas odio ac massa sodales hendrerit. 
+                        </p>
+
+                        <p class="lead" style="margin-left:4px;">Tags: 
+                            <a href="#">tag 4</a>
+                            <a href="#">tag 5</a>
+                            <a href="#">tag 6</a>
+                        </p>
+                    </div>
+                    <!-- fin -->
+
+                </div>
+
+                <div class="span4">
+                    <h2> Categories by tags </h2>
+                    <ul class="nav nav-list">
+                        <li><a href="#">tag 1</a></li>
+                        <li><a href="#">tag 2</a></li>
+                        <li><a href="#">tag 3</a></li>
+                        <li><a href="#">tag 4</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+
+             
+              <div class="container">
+                <p class="muted credit">Our blog is made by us, doh <a href="#">Copyright®</a> In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis porta blandit, diam nibh porta ante, et commodo erat eros vitae nibh. Nulla</p>
+              </div>
+             
+
+        </div>
+
         <!-- jQuery -->
         <script src="http://code.jquery.com/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
+    
     </body>
 
 </html>
