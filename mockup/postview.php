923f019c9c783335ae8963f1cbd3cc61ce70c02e
Marius-Cristian Manole <manole.cristi@gmail.com>
2013-05-06 17:03:03 +0100

    Added single post view + comments + comm form

---
+++ mockup/postview.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Blog Homepage</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/style.css" rel="stylesheet" media="screen">
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="../assets/js/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+        
+
+
+    <body>
+
+        <div class="navbar">
+            <div class="navbar-inner">
+                <div class="container">
+                    <div class="nav">
+                        <a class="brand" href="index.php"> Our first blog </a>
+                        <ul class="nav">
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="index.php">Link to ?</a></li>
+                            <li class="active"><a href="index.php">Link to ?</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+
+        
+            <div class="row">
+                <div class="span12 divpost img-polaroid">
+
+                        <h3 class='post'>Post title goes here</h2>
+                        <small class='content'>03 May 2013</small><br>
+                        <small class='content'>by&nbsp;<a href="#">Nelly Furtado</a></small>
+                        <hr/>
+                        
+                        <p class="content">
+                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam porttitor suscipit est, eu condimentum nisi commodo et. Vivamus quis purus erat. Donec consectetur tortor vitae neque consequat ac tempor ipsum tincidunt. Nullam pellentesque nulla sed felis elementum pellentesque. Quisque fringilla erat quis purus sagittis eu vulputate neque faucibus. Mauris at venenatis urna. Nulla in libero ipsum. Vivamus purus velit, convallis sed facilisis et, rutrum vitae nulla. Integer consequat elit at justo rhoncus accumsan.
+                            Phasellus sagittis hendrerit placerat. In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis porta blandit, diam nibh porta ante, et commodo erat eros vitae nibh. Nulla facilisi. Aenean eget sem a augue ultrices interdum. Nunc id mattis purus. Ut vulputate mollis augue a laoreet. Fusce mollis molestie erat, non vulputate sapien tincidunt sit amet. Praesent lobortis purus at tortor hendrerit sed aliquam purus adipiscing. Aliquam ante lacus, molestie id mattis quis, faucibus quis ante. Pellentesque odio elit, tincidunt sed luctus ac, blandit sit amet metus. Maecenas quis massa odio, quis tempus nulla. Aenean eros dolor, commodo id pretium nec, faucibus at velit. Aenean egestas odio ac massa sodales hendrerit. 
+                        </p>
+
+                        <p class="tags">Tags: 
+                            <a href="#">tag 1</a>
+                            <a href="#">tag 2</a>
+                            <a href="#">tag 3</a>
+                        </p>
+          
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="span12 divpost img-polaroid">
+                    <h3 class="post"> Comments </h3>
+                    
+                    <div class="row">
+                        <div class="span12">
+                           
+                            <h4 class="post"><a href="#">Queen Margaret</a></h4>
+                            <div class="atooltip"></div>
+                            <p class="content comment">
+                                 rutrum vitae nulla. Integer consequat elit at justo rhoncus accumsan.
+                                Phasellus sagittis hendrerit placerat. In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis  porta blandit, d
+                            </p> 
+                            <div class="anothertooltip"></div>
+                        
+                        </div>
+                    </div>
+
+                    <!-- another 2 -->
+                    <div class="row">
+                        <div class="span12">
+                           
+                            <h4 class="post"><a href="#">Shaq O'neal</a></h4>
+                            <div class="atooltip"></div>
+                            <p class="content comment">
+                                 rutrum vitae nulla. Integer consequat elit at justo rhoncus accumsan.
+                                Phasellus sagittis hendrerit placerat. In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis  porta blandit, d
+                            </p> 
+                            <div class="anothertooltip"></div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="span12">
+                           
+                            <h4 class="post"><a href="#">Bill Jobs</a></h4>
+                            <div class="atooltip"></div>
+                            <p class="content comment">
+                                 rutrum vitae nulla. Integer consequat elit at justo rhoncus accumsan.
+                                Phasellus sagittis hendrerit placerat. In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis  porta blandit, d
+                            </p> 
+                            <div class="anothertooltip"></div>
+                        </div>
+                    </div>
+                    <!-- end of another 2-->
+
+                    <div class="row">
+                        <div class="span12">
+                            <h3 class="post"></h3>
+
+                            <form action="" method="" class="form-horizontal">
+                                <legend class="post">Comment and contribute</legend>
+                                
+                                <div class="control-group"> 
+                                    
+                                    <label class="control-label">Your Name:</label>
+                                    
+                                    <div class="controls">
+                                        <input type="text" name="" placeholder="> ">
+                                    </div>
+                                
+                                </div>
+
+                                <div class="control-group">
+
+                                    <label class="control-label">Comment:</label>
+                                    
+                                    <div class="controls">
+                                        <textarea rows="9" class="input-xxlarge"></textarea>
+                                    </div>
+                                    
+                                </div>
+
+                                    <div class="controls">
+                                        <button type="submit" class="btn btn-inverse">Publish</button>
+                                    </div>
+                                    
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+             
+              <div class="footer container">
+                <p class="muted credit">Our blog is made by us, doh <a href="#">Copyright®</a> In consectetur mollis dolor vitae cursus. Nulla iaculis, velit quis porta blandit, diam nibh porta ante, et commodo erat eros vitae nibh. Nulla</p>
+              </div>
+             
+
+        </div>
+
+        <!-- jQuery -->
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    
+    </body>
+
+</html>
